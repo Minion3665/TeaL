8e145f05014f267e56fff614386c2ece1de7b5bf
Skyler Grey <skyler3665@gmail.com>
2022-09-29 14:03:51 +0100

    Fix the remove task test
    - IDs start from 1 not 0

--- src/database.rs
+++ src/database.rs
@@ -8,7 +8,7 @@ pub struct Database {
 
 #[derive(Debug)]
 pub struct Task {
-    pub id: i64,
+    pub id: i64,  // IMPORTANT: This begins from 1, *not* 0
     pub description: String
 }
 
@@ -30,10 +30,51 @@ impl Database {
         sqlx::query!("INSERT INTO tasks VALUES (null, ?)", task).execute(&mut self.connection).await
     }
 
-    pub fn remove_task(index: i32) {}
+    pub async fn remove_task(&mut self, index: i32) -> Result<u64, Error> {
+        Ok(sqlx::query!("DELETE FROM tasks WHERE id = ?", index).execute(&mut self.connection).await?.rows_affected())
+    }
 
     pub async fn list_tasks(&mut self) -> Result<Vec<Task>, Error>{
         sqlx::query_as!(Task, "SELECT * FROM tasks").fetch_all(&mut self.connection).await
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn add_task_test() {
+        let mut db = Database::new().await.unwrap();
+
+        db.setup().await.unwrap();
+
+        assert_eq!(db.list_tasks().await.unwrap().len(), 0);
+
+        db.add_task("A test task").await.unwrap();
+
+        assert_eq!(db.list_tasks().await.unwrap().len(), 1);
+    }
+
+    #[tokio::test]
+    async fn remove_task_test() {
+        let mut db = Database::new().await.unwrap();
+
+        db.setup().await.unwrap();
+
+        assert_eq!(db.list_tasks().await.unwrap().len(), 0);
+
+        db.add_task("A test task").await.unwrap();
+
+        for task in db.list_tasks().await.unwrap() {
+            println!("Task got ID {}", task.id);
+        }
+
+        assert_eq!(db.list_tasks().await.unwrap().len(), 1);
+
+        assert_eq!(db.remove_task(1).await.unwrap(), 1);
+        db.remove_task(0).await.unwrap();
+
+        assert_eq!(db.list_tasks().await.unwrap().len(), 0);
+    } 
+}
