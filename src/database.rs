c60c8e957c22ac80348129e43a0c51c363f5c025
Skyler Grey <skyler3665@gmail.com>
2022-12-01 01:02:29 +0000

    Add a basic TUI to display tasks

--- src/database.rs
+++ src/database.rs
@@ -1,15 +1,15 @@
-use sqlx::{SqliteConnection, Connection, Error, sqlite::SqliteQueryResult};
+use sqlx::{sqlite::SqliteQueryResult, Connection, Error, SqliteConnection};
 
 // Schema is on <app.dbdesigner.net>
 
 pub struct Database {
-    connection: SqliteConnection
+    connection: SqliteConnection,
 }
 
 #[derive(Debug)]
 pub struct Task {
-    pub id: i64,  // IMPORTANT: This begins from 1, *not* 0
-    pub description: String
+    pub id: i64, // IMPORTANT: This begins from 1, *not* 0
+    pub description: String,
 }
 
 impl Database {
@@ -20,22 +20,33 @@ impl Database {
     }
 
     pub async fn setup(&mut self) -> Result<SqliteQueryResult, Error> {
-        sqlx::query("CREATE TABLE tasks (
+        sqlx::query(
+            "CREATE TABLE tasks (
             id integer PRIMARY KEY AUTOINCREMENT,
             description text NOT NULL
-        )").execute(&mut self.connection).await
+        )",
+        )
+        .execute(&mut self.connection)
+        .await
     }
 
     pub async fn add_task(&mut self, task: &str) -> Result<SqliteQueryResult, Error> {
-        sqlx::query!("INSERT INTO tasks VALUES (null, ?)", task).execute(&mut self.connection).await
+        sqlx::query!("INSERT INTO tasks VALUES (null, ?)", task)
+            .execute(&mut self.connection)
+            .await
     }
 
     pub async fn remove_task(&mut self, index: i32) -> Result<u64, Error> {
-        Ok(sqlx::query!("DELETE FROM tasks WHERE id = ?", index).execute(&mut self.connection).await?.rows_affected())
+        Ok(sqlx::query!("DELETE FROM tasks WHERE id = ?", index)
+            .execute(&mut self.connection)
+            .await?
+            .rows_affected())
     }
 
-    pub async fn list_tasks(&mut self) -> Result<Vec<Task>, Error>{
-        sqlx::query_as!(Task, "SELECT * FROM tasks").fetch_all(&mut self.connection).await
+    pub async fn list_tasks(&mut self) -> Result<Vec<Task>, Error> {
+        sqlx::query_as!(Task, "SELECT * FROM tasks")
+            .fetch_all(&mut self.connection)
+            .await
     }
 }
 
@@ -76,5 +87,5 @@ mod tests {
         db.remove_task(0).await.unwrap();
 
         assert_eq!(db.list_tasks().await.unwrap().len(), 0);
-    } 
+    }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,14 @@
-use std::io;
+use std::io::{self, stdout};
 
-use sqlx::Error;
 use crate::database;
+use crossterm::{self, event::{EnableMouseCapture, Event, KeyEvent, KeyCode, read, DisableMouseCapture}, execute, terminal::{EnterAlternateScreen, LeaveAlternateScreen, self}, ExecutableCommand};
+use sqlx::Error;
+use tui::{
+    backend::CrosstermBackend,
+    symbols::block,
+    widgets::{self, Block, Borders},
+    Terminal,
+};
 
 #[derive(PartialEq)]
 pub enum DisplayingTasksStates {
@@ -11,15 +18,50 @@ pub enum DisplayingTasksStates {
 
 #[derive(PartialEq)]
 pub enum States {
-    DisplayingTasks(DisplayingTasksStates)
+    DisplayingTasks(DisplayingTasksStates),
 }
 
 pub async fn display_tasks(db: &mut database::Database) -> Result<(), Error> {
     let tasks = db.list_tasks().await?;
 
+    let mut list_items = Vec::new();
+
     for task in tasks {
-        println!("[{}]: {}", task.id, task.description);
-    };
+        list_items.push(widgets::ListItem::new(format!(
+            "[{}]: {}",
+            task.id, task.description
+        )))
+    }
+
+    let list = widgets::List::new(list_items);
+
+    crossterm::terminal::enable_raw_mode()?;
+    execute!(stdout(), EnterAlternateScreen, EnableMouseCapture)?;
+    let mut backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Your tasks").borders(Borders::ALL);
+
+        let inner_area = block.inner(size);
+
+        f.render_widget(block, size);
+        f.render_widget(list, inner_area);
+    })?;
+
+    loop {
+        match read()? {
+            Event::Key(event) => match event.code {
+                KeyCode::Char('q') => break,
+                _ => continue
+            }
+            _ => continue
+        }
+    }
+
+    execute!(stdout(), LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
     Ok(())
 }
